26b3df3d6ab3f5122248265cda3331dcaf084f49
HouAng <l>
2020-03-06 22:23:43 +0800

    E4 Finish

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,11 @@
 Page({
-  
+  onTap: function () {
+    // wx.navigateTo({
+    //   url: '/pages/posts/posts',
+    // })
+    //redirectTo 不能返回 页面会被unload navigateTo 可以返回 页面 hide
+    wx.redirectTo({
+      url: '/pages/posts/posts',
+    })
+  }
 })
