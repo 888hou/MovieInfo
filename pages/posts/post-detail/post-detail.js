186a55f55f8e93c354ceee3c78506d6f974e4fb7
HouAng <l>
2020-03-10 14:46:00 +0800

    6-12OK

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -6,12 +6,13 @@ Page({
    * Page initial data
    */
   data: {
+    isPlayImgMusic: false
   },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var postId = options.id;
     this.data.currentPostId = postId;
     this.setData({
@@ -19,14 +20,14 @@ Page({
     })
     //从缓存取数据
     var postsCollected = wx.getStorageSync('posts_collected');
-    if (postsCollected){
+    if (postsCollected) {
       var postCollected = postsCollected[postId];
-      if (postCollected){
+      if (postCollected) {
         this.setData({
           collected: postCollected
         })
       }
-    }else{//如果缓存没有创建缓存
+    } else { //如果缓存没有创建缓存
       var postsCollected = {};
       postsCollected[postId] = false;
       wx.setStorageSync("posts_collected", postsCollected);
@@ -35,18 +36,32 @@ Page({
       collected: postsCollected[postId]
     });
   },
-  onPlayMusicTap: function () {
+  onMusicTap: function() {
+    if (this.data.isPlayImgMusic) {
+      wx.pauseBackgroundAudio();
+      console.log('stop')
+      this.setData({
+        isPlayImgMusic: false
+      });
+    } else {
+      wx.playBackgroundAudio({
+        dataUrl: this.data.music.url,
+        title: this.data.music.title,
+        coverImgUrl: this.data.music.coverImg
+      })
+      this.setData({
+        isPlayImgMusic: true
+      });
+      console.log('start')
+    }
     console.log(this.data.music.url);
-    wx.playBackgroundAudio({
-      dataUrl: this.data.music.url,
-    })
   },
-  onCollectionTap: function (event) {
+  onCollectionTap: function(event) {
     //this.getPostsCollectedSync();
     this.getPostsCollectedAsync();
   },
 
-  getPostsCollectedAsync: function () {
+  getPostsCollectedAsync: function() {
     //从缓存取数据
     var _this = this;
     var postsCollected = wx.getStorage({
@@ -61,10 +76,10 @@ Page({
         _this.showToast(postsCollected, postCollected);
       }
     })
-    
+
   },
 
-  getPostsCollectedSync: function(){
+  getPostsCollectedSync: function() {
     //从缓存取数据
     var postsCollected = wx.getStorageSync('posts_collected');
     var postCollected = postsCollected[this.data.currentPostId];
@@ -77,18 +92,18 @@ Page({
     this.showToast(postsCollected, postCollected);
   },
 
-  showModal: function (postsCollected, postCollected) {
+  showModal: function(postsCollected, postCollected) {
     var _this = this;
     wx.showModal({
       title: '收藏',
       content: postCollected ? "是否收藏该文章" : "是否取消收藏",
-      showCancel : true,
+      showCancel: true,
       cancelText: postCollected ? "不收藏" : "返回",
       cancelColor: "#333",
       confirmText: postCollected ? "收藏" : "取消收藏",
       confirmColor: "#405f80",
       success: function(res) {
-        if(res.confirm){
+        if (res.confirm) {
           wx.setStorageSync('posts_collected', postsCollected);
           _this.setData({
             collected: postCollected
@@ -97,7 +112,7 @@ Page({
       }
     });
   },
-  showToast: function (postsCollected, postCollected) {
+  showToast: function(postsCollected, postCollected) {
     wx.setStorageSync('posts_collected', postsCollected);
     this.setData({
       collected: postCollected
@@ -108,13 +123,13 @@ Page({
       duration: 1000
     });
   },
-  onShareTap: function (event) {
+  onShareTap: function(event) {
     var itemList = ['分享给微信好友', '分享到朋友圈', '分享到QQ', '分享到朋友圈']
     wx.showActionSheet({
       itemList: itemList,
       itemColor: '#405f80',
-      success:function(res) {
-        if(!res.cancel){
+      success: function(res) {
+        if (!res.cancel) {
           console.log(res.tapIndex);
         }
       }
@@ -124,49 +139,49 @@ Page({
   /**
    * Lifecycle function--Called when page is initially rendered
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page show
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page hide
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page unload
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * Page event handler function--Called when user drop down
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * Called when page reach bottom
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * Called when user click on the top right corner to share
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
